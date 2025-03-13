594508596f73d0c4a24db3fbe646e20c31759e95
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-03-13 11:56:48 +0100

    feat: implemented detail screen with routes and back button

--- frontend/src/app/blog-screen/blog-screen.component.ts
+++ frontend/src/app/blog-screen/blog-screen.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BlogEntry } from '../models/blog-entry.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-screen',
@@ -12,14 +12,18 @@ export class BlogScreenComponent {
   selectedBlogEntry: BlogEntry | undefined;
 
   blogEntries: BlogEntry[] = [
-    { id: 1, title: 'Erster Artikel', author_id: 101, description:"Beschreibung 1", content_text: 'Hier steht der komplette Artikelinhalt...' },
+    { id: 1, title: 'Erster Artikel', author_id: 101, description:"Beschreibung 1", content_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
     { id: 2, title: 'Zweiter Artikel', author_id: 102, description:"Beschreibung 2", content_text: 'Hier steht ein weiterer ausführlicher Inhalt...' }
   ];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.selectedBlogEntry = this.blogEntries.find(entry => entry.id === id);
+  }
+
+  goBack(){
+    this.router.navigate(['/']);
   }
 }
