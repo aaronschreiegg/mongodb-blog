25fe32c5f97cc9af52e9c5084645837eaf42325e
MistelbergerLeopold <l.mistelberger@students.htl-leonding.ac.at>
2025-03-20 10:50:46 +0100

    fix: fixed the id in the test Data

--- init/import_data.js
+++ init/import_data.js
@@ -19,7 +19,9 @@ const users = [
 
 db.BlogUser.insertMany(users);
 
-const userIds = db.BlogUser.find().map(user => user._id);
+const userIds = db.BlogUser.find().toArray().map(user => user._id);
+
+
 
 const categories = [
     { name: "Technology" },
@@ -28,54 +30,50 @@ const categories = [
 ];
 db.BlogCategory.insertMany(categories);
 
-const categoryIds = db.BlogCategory.find().map(cat => cat._id);
 
 const blogEntries = [
     {
         title: "MongoDB Basics",
         author_ids: [userIds[0], userIds[3]],
         description: "An introduction to MongoDB.",
-        creationDate: new Date(),
-        editDates: [],
+        creationDate: new Date(2025, 1, 14),
+        editDates: [new Date(2025, 2, 11)],
         impressionCount: 10,
         commentsAllowed: true,
         content_text: "MongoDB is a NoSQL database...",
         content_images: [imagesBase64[0]],
-        category_id: categoryIds[0]
     },
     {
         title: "Best Travel Destinations",
         author_ids: [userIds[2]],
         description: "The best places to visit in 2025.",
-        creationDate: new Date(),
-        editDates: [],
+        creationDate: new Date(2025, 1, 17),
+        editDates: [new Date(2025, 2, 4), new Date(2025, 2, 5)],
         impressionCount: 25,
         commentsAllowed: true,
         content_text: "If you're planning your next vacation...",
         content_images: [imagesBase64[1]],
-        category_id: categoryIds[1]
     },
     {
         title: "Delicious Recipes",
         author_ids: [userIds[4]],
         description: "Try these amazing dishes at home.",
-        creationDate: new Date(),
-        editDates: [],
+        creationDate: new Date(2025, 1, 19),
+        editDates: [new Date(2025, 1, 22)],
         impressionCount: 5,
         commentsAllowed: false,
         content_text: "Cooking is an art...",
-        content_images: [imagesBase64[3]],
-        category_id: categoryIds[2]
+        content_images: [imagesBase64[3], imagesBase64[4]],
     }
 ];
 db.BlogEntry.insertMany(blogEntries);
 
-const blogIds = db.BlogEntry.find().map(blog => blog._id);
+const blogIds = db.BlogUser.find().toArray().map(blog => blog._id);
 
 const comments = [
     { blog_entry_id: blogIds[0], user_id: userIds[1], content_text: "Great article!" },
     { blog_entry_id: blogIds[0], user_id: userIds[2], content_text: "Very helpful, thanks!" },
-    { blog_entry_id: blogIds[1], user_id: userIds[0], content_text: "I want to visit these places!" },
+    { log_entry_id: blogIds[1], user_id: userIds[0], content_text: "I want to visit these places!" },
     { blog_entry_id: blogIds[1], user_id: userIds[3], content_text: "Nice recommendations!" }
 ];
 db.Comment.insertMany(comments);
